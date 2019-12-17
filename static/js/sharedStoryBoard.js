d91c48e5a0da591f59d79569bba5a741e49901e8
hominhhoa39 <hominhhoa39@gmail.com>
2019-12-18 00:19:25 +0900

    Commit self-learning samples

---
+++ static/js/sharedStoryBoard.js
@@ -0,0 +1,13 @@
+function initViz() {
+    var containerDiv = document.getElementById("vizContainer"),
+        url = "https://eu-west-1a.online.tableau.com/t/minhoa/views/GlobalSuperstore/Story1?:origin=card_share_link&:embed=n",
+        options = {
+            hideTabs: true,
+			hideToolbar: true,
+            onFirstInteractive: function () {
+                console.log("Run this code when the viz has finished loading.");
+            }
+        };
+
+    var viz = new tableau.Viz(containerDiv, url, options);
+}
